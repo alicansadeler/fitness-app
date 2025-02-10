47e4baab491ae9d89a068f7a81551b971748438b
alicansadeler <alicansadeler@gmail.com>
2025-02-11 00:03:32 +0300

    navbar güncellendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./screen/navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <div>
+        <Navbar></Navbar>
+      </div>
       <p className="text-2xl">Ali Can SADELER</p>
     </>
   );
---
+++ src/screen/navbar/index.tsx
@@ -0,0 +1,47 @@
+import logo from "../../assets/logo.png";
+import Link from "./link";
+import { SelectedPage } from "../../shared/types";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({ setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  return (
+    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} w-full gap-16`}>
+          <img src={logo} alt="logo" />
+
+          <div className={`${flexBetween} w-full`}>
+            <div className={`${flexBetween} mt-4 gap-8 text-sm`}>
+              <Link
+                page="Home"
+                selectedPage={SelectedPage.Home}
+                setSelectedPage={setSelectedPage}
+              ></Link>
+              <Link
+                page="Benefits"
+                selectedPage={SelectedPage.Benefits}
+                setSelectedPage={setSelectedPage}
+              ></Link>
+              <Link
+                page="Our Classes"
+                selectedPage={SelectedPage.OurClasses}
+                setSelectedPage={setSelectedPage}
+              ></Link>
+              <Link
+                page="Contact Us"
+                selectedPage={SelectedPage.ContactUs}
+                setSelectedPage={setSelectedPage}
+              ></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
