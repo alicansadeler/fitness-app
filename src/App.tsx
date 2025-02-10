4c93a00f2a088c7c07670695ceaffacf9b5075dc
alicansadeler <alicansadeler@gmail.com>
2025-02-10 21:16:44 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-2xl">Ali Can SADELER</p>
+    </>
+  );
+}
+
+export default App;
