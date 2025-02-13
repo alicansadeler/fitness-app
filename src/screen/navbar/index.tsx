8f7efba6ef10b82aeaa4501b18cab6d1492d5d9d
alicansadeler <alicansadeler@gmail.com>
2025-02-13 14:09:18 +0300

    become button ve mobile menü eklendi

--- src/screen/navbar/index.tsx
+++ src/screen/navbar/index.tsx
@@ -1,60 +1,117 @@
-import logo from "../../assets/logo.png";
+import React, { useState } from "react";
+import Logo from "../../assets/logo.png";
 import Link from "./Link";
 import { SelectedPage } from "../../shared/types";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import ActionButton from "../../shared/ActionButton";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import useMediaQuery from "../../hooks/useMediaQuery";
-import { Bars3Icon } from "@heroicons/react/16/solid";
 
 type Props = {
-  setSelectedPage: (value: SelectedPage) => void;
   isTopOfPage: boolean;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Navbar = ({ isTopOfPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
-  const navBarBackground = isTopOfPage
+  const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
+  const navBarBackGround = isTopOfPage
     ? ""
     : "bg-primary-100 drop-shadow shadow-lg";
   return (
-    <div
-      className={`${flexBetween} ${navBarBackground} fixed top-0 z-30 w-full py-6`}
-    >
-      <div className={`${flexBetween} mx-auto w-5/6`}>
-        <div className={`${flexBetween} w-full gap-16`}>
-          <img src={logo} alt="logo" />
-          {isAboveMediumScreens ? (
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} mt-4 gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={SelectedPage.Home}
-                  setSelectedPage={setSelectedPage}
-                ></Link>
-                <Link
-                  page="Benefits"
-                  selectedPage={SelectedPage.Benefits}
-                  setSelectedPage={setSelectedPage}
-                ></Link>
-                <Link
-                  page="Our Classes"
-                  selectedPage={SelectedPage.OurClasses}
-                  setSelectedPage={setSelectedPage}
-                ></Link>
-                <Link
-                  page="Contact Us"
-                  selectedPage={SelectedPage.ContactUs}
-                  setSelectedPage={setSelectedPage}
-                ></Link>
+    <nav>
+      <div
+        className={`${flexBetween} ${navBarBackGround} fixed top-0 z-30 w-full py-6`}
+      >
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img src={Logo} />
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full `}>
+                <div className={`${flexBetween} mt-2 gap-8 text-sm  `}>
+                  <Link
+                    page="Home"
+                    selectedPage={SelectedPage}
+                    setSelectedPage={setSelectedPage}
+                  ></Link>
+
+                  <Link
+                    page="Benefits"
+                    selectedPage={SelectedPage}
+                    setSelectedPage={setSelectedPage}
+                  ></Link>
+
+                  <Link
+                    page="Our Classes"
+                    selectedPage={SelectedPage}
+                    setSelectedPage={setSelectedPage}
+                  ></Link>
+
+                  <Link
+                    page="Contact Us"
+                    selectedPage={SelectedPage}
+                    setSelectedPage={setSelectedPage}
+                  ></Link>
+                </div>
+                <div className={`${flexBetween}  mt-2 gap-8 `}>
+                  <p className="font-dmssans text-sm">Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
               </div>
-            </div>
-          ) : (
-            <button className="rounded-full bg-secondary-500 p-2">
-              <Bars3Icon className="w-6 h-6"></Bars3Icon>
-            </button>
-          )}
+            ) : (
+              <button className="rounded-full bg-secondary-500 p-2">
+                <Bars3Icon
+                  onClick={() => setIsMenuToggle(!isMenuToggle)}
+                  className="w-6 h-6"
+                ></Bars3Icon>
+              </button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      {!isAboveMediumScreens && isMenuToggle && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-72 bg-primary-100 drop-shadow shadow-lg">
+          <div className="flex justify-end p-10">
+            <button className="rounded-full ">
+              <XMarkIcon
+                onClick={() => setIsMenuToggle(!isMenuToggle)}
+                className="w-6 h-6"
+              ></XMarkIcon>
+            </button>
+          </div>
+
+          <div className="ml-11 text-2xl flex flex-col gap-10">
+            <Link
+              page="Home"
+              selectedPage={SelectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+
+            <Link
+              page="Benefits"
+              selectedPage={SelectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+
+            <Link
+              page="Our Classes"
+              selectedPage={SelectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+
+            <Link
+              page="Contact Us"
+              selectedPage={SelectedPage}
+              setSelectedPage={setSelectedPage}
+            ></Link>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 };
 
---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ActionButton = ({ children, setSelectedPage }: Props) => {
+  return (
+    <AnchorLink className="bg-secondary-500 rounded-lg py-2 px-10 hover:bg-primary-500 hover:text-white transition duration-300 hover:duration-500 text-center">
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
